637e56e7d33a3a6bdce1cde2d0e7f6c9f398d4e5
victoresch <victoresch@hotmail.com>
2019-08-20 09:53:51 -0500

    envia string por get

--- app/src/main/java/samples/samuems/MainActivity.java
+++ app/src/main/java/samples/samuems/MainActivity.java
@@ -1,9 +1,18 @@
 package samples.samuems;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.speech.RecognizerIntent;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -35,6 +44,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int permisionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permisionCheck== PackageManager.PERMISSION_DENIED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+
+            }else{
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+            }
+
+        }
         mEntradaVoz = findViewById(R.id.textoEntrada);
         mBotonHablar = findViewById(R.id.botonHablar);
         retrofit = new Retrofit.Builder()
@@ -62,18 +80,33 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @SuppressLint("MissingPermission")
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        LocationManager locManager;
+        Location loc;
+        locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        loc = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        System.out.println("Longitud: "+loc.getLongitude()+ " Latitud: "+loc.getLatitude());
+        Toast toast1 = Toast.makeText(getApplicationContext(),"Coordenadas:"+loc.getLongitude()+" - "+loc.getLatitude(), Toast.LENGTH_LONG);
+        toast1.show();
+
+
         super.onActivityResult(requestCode, resultCode, data);
         switch (requestCode) {
             case REQ_CODE_SPEECH_INPUT: {
                 if (resultCode == RESULT_OK && null != data) {
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     mEntradaVoz.setText(result.get(0));
-                    System.out.println(result.get(0));
+
                     //Para Enviar los datos
                     enviarServidorGet(result.get(0));
                     //enviarServidorPost(result.get(0));
+
+
+                    System.out.println(result.get(0));
+
 
                 }
                 break;
